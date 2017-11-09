1f1f0c8a725effaa3de8049089e5a9e5604fc19b
arthurzakaryan95 <arthurzakaryan95@mail.ru>
2017-11-09 20:22:42 +0400

    WC js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-$(".show").on("mouseover",function(){
-	$(".hidee").show(3000);
-});
-$(".show").on("mouseout",function(){
-	$(".hidee").hide(3000);
-});
+ $(".show").on("mouseover",function(){
+	$(".hidee").toggle(5000);
+});  
